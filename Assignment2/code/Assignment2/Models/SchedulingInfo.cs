1340e60fffb0687cb587c87a4b66089b42276210
Abdul-Rahman <44008172+AbdulBarakeh@users.noreply.github.com>
2024-01-23 08:01:32 +0100

    init scheduling assignment

---
+++ Assignment2/code/Assignment2/Models/SchedulingInfo.cs
@@ -0,0 +1,8 @@
+public class SchedulingInfo
+{
+    public string TeacherInitials {get; set;}
+    public string Classname { get; set; }
+    public string Subject { get; set; }
+    public DateTime Date { get; set; }
+    public string Classroom {get; set;}
+}

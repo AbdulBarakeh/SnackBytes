3251243638589e329cf3913d7a5a75888d1cdd99
Abdul-Rahman <44008172+AbdulBarakeh@users.noreply.github.com>
2024-01-23 07:59:43 +0100

    WIP

---
+++ Assignment1/Code/BlazoreVersion/BlazorApp1/Logic/CatfactRetriever.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorApp1.Logic
+{
+    public class CatfactRetriever
+    {
+        public HttpClient httpClient { get; set; }
+        public CatfactRetriever(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+        public async Task<string> RetrieveFact()
+        {
+            var response = await httpClient.GetStringAsync("https://catfact.ninja/fact");
+            var content = response;
+            var fact = content;
+            return fact; 
+        }
+    }
+}

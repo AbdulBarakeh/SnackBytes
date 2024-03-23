b28b23a5a0ef500142b85f86fc5ccca2ee7a5abf
Abdul <abdulbarakeh98@gmail.com>
2024-03-23 15:09:41 +0100

    Fix bugs

--- Assignment2/code/SchedulingApp/SchedulingApp.Shared/Services/SchedulingService.cs
+++ Assignment2/code/SchedulingApp/SchedulingApp.Shared/Services/SchedulingService.cs
@@ -6,13 +6,16 @@ public class SchedulingService
     public static List<SchedulingInfo> MapCSV(string inputPath)
     {
         var lines = File.ReadAllLines(inputPath);
-        var mappedData = lines.ToList().Skip(1).Select(row => new SchedulingInfo
-        {
-            TeacherInitials = row[0].ToString()
-            ,Classname = row[1].ToString()
-            ,Subject = row[2].ToString()
-            ,Date = DateTime.Parse(row[3].ToString())
-            ,Classroom = row[4].ToString()
+        DateTime parsedDate;
+        var mappedData = lines.ToList().Skip(1).Select(row => {
+            var data = row.Split(',');
+            return new SchedulingInfo{
+            TeacherInitials = data[0].ToString()
+            ,Classname = data[1].ToString()
+            ,Subject = data[2].ToString()
+            ,Date = DateTime.TryParseExact(data[3].ToString(), "yyyy-MM-dd", null, System.Globalization.DateTimeStyles.None, out parsedDate)?parsedDate:DateTime.Now
+            ,Classroom = data[4].ToString()
+            };
         });
         return mappedData.ToList();
     }

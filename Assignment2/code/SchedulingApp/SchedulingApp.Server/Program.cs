e748b773c34b355d75731e9a2de05ba55bb3cee3
Abdul <abdulbarakeh98@gmail.com>
2024-05-05 19:24:47 +0200

    Make app compatible with wasm and server rander modes.... again

--- Assignment2/code/SchedulingApp/SchedulingApp.Server/Program.cs
+++ Assignment2/code/SchedulingApp/SchedulingApp.Server/Program.cs
@@ -1,19 +1,13 @@
-using Microsoft.AspNetCore.ResponseCompression;
-using SchedulingApp.Server;
-using SchedulingApp.Server.Hubs;
+using SchedulingApp.Client;
 using SchedulingApp.Shared.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
 
-builder.Services.AddResponseCompression(opts =>
-{
-    opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
-          new[] { "application/octet-stream" });
-});
 builder.Services.AddSingleton<ISchedulingService, SchedulingService>();
 var app = builder.Build();
 
@@ -28,14 +22,13 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-app.UseResponseCompression();
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 app.UseAntiforgery();
-app.MapHub<CalendarHub>("/calendarhub");
 app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode();
 var schedulingService = app.Services.GetRequiredService<ISchedulingService>();
 schedulingService.MapCSV("wwwroot\\Data\\input.csv");
 app.Run();
